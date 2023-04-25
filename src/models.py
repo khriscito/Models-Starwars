4c565d75b1acb70b49d18f73bd0eb84650fbe8e2
khriscito <khristhopher100@hotmail.com>
2023-04-25 23:13:38 +0000

    starwars data model

--- src/models.py
+++ src/models.py
@@ -7,23 +7,57 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class user(Base):
+    __tablename__ = 'user'
+    id= Column(Integer,primary_key=True)
+    username= Column(String(20), nullable=False)
+    email= Column(String(30), nullable=False)
+    password= Column(String(20), nullable=False)
+    profile = relationship('profile')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class profile(Base):
+    __tablename__ = 'profile'
+    id= Column(Integer,primary_key=True)
+    name= Column(String(20), nullable=False)
+    following=Column(String(50), nullable=False)
+    followers=Column(String(50), nullable=False)
+    profile_photo= Column(String(50), nullable=True)
+    profile_description=Column(String(50), nullable=True)
+    publications = Column(String(50), nullable=True)
+    add_publication= Column(String(100), nullable=False)
+    user=relationship(user)
+    user_id= Column(Integer,ForeignKey('user.id'),nullable=False)
+    followers= relationship('followers')
+    posts = relationship('post')
+    comment = relationship('comment')
+
+class followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer,primary_key=True)
+    user_from_id= Column(Integer,nullable=False)
+    user_to_id= Column(Integer,nullable=False)
+    user=relationship(user)
+    user_id= Column(Integer,ForeignKey('user.id'), nullable=False)
+
+class posts (Base):
+    __tablename__='posts'
+    id= Column(Integer,primary_key=True)
+    post_id= Column(Integer,nullable=False)
+    url = Column(String(50),nullable=False)
+    user=relationship(user)
+    user_id= Column(Integer,ForeignKey('user.id'), nullable=False)
+    comment= relationship('comment')
+
+class comment (Base):
+    __tablename__= "comment"
+    id= Column(Integer,primary_key=True)
+    comment_id= Column(Integer,nullable=False)
+    comment_text= Column(String(200),nullable=False)
+    user=relationship(user)
+    user_id= Column(Integer,ForeignKey('user.id'), nullable=False)
+    posts = relationship('post')
+
+
 
     def to_dict(self):
         return {}
